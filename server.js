a79229ca29a1de1bda9a4a2fbff521f0d386c158
Samriddhiy <samriddhi4005@gmail.com>
2024-11-19 23:43:17 +0530

    connected

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import { Server } from "socket.io"; 
+import { fileURLToPath } from "url";
+import { dirname , join } from "path";
+
+
 dotenv.config({
     path: './.env',
 })
-
+import { createServer } from "http";
 const app = express ();
+
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.get("/" , (req, res) => {
+    console.log("Got Request /");
+    res.sendFile(join (__dirname + "/app/index.html"));
+})
+
+io.on("connection", (socket) => {
+    console.log(`Someone conneccted to socket server and socket id is ${socket.id}`);
+    //socket.emit()
+})
+
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is listening on port: ${process.env.PORT}`);
